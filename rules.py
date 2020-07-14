77d2061658edbb8942c6c70f2dee93a51a1bfc0a
nosnowowie93347 <65568190+nosnowowie93347@users.noreply.github.com>
2020-07-14 12:02:42 -0700

    Delete rules.py

--- rules.py
+++
@@ -1,9 +0,0 @@
-'''1. Use correct channels\n 2. Behave (use common sense)\n 3. Only Devs/admins
-are allowed to ban bots]\n 4. #broken-commands  for commands not working.\n 
-6. #mute-appeal for unfair mutes\n
-7. Spamming is reccomended in #spamming (this includes spam emojis)\n
-8. (Rule only applies to @Pinkalicious21902 and their alts): No adding people that I invite to the New People role\n
-9. Me, Ruby, and any admin WILL be occasionally purging channels\n
-1O. No longer have a alt limit!! bring as many as u want!!\n
-11. NO SWEARING. @thicc boi ( ͡° ͜ʖ ͡°)  WILL DELETE IT.\n
-12. NO NSFW WHATSOEVER\n'''
